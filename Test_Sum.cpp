ae26ae19501ce94d8b90d5e38d41993b269093fc
Shuvam Chakraborty <91843555+Shuvam-Chakraborty@users.noreply.github.com>
2023-03-18 17:55:19 +0530

    Testing Source Code

---
+++ Test_Sum.cpp
@@ -0,0 +1,10 @@
+/*This program add up two numbers*/
+#include<stdio.h>
+
+int main()
+{//Too many comments
+	int a=2/*Hello*/, b;
+	scanf("%d",&b);
+	printf("Sum : %d" , a + b);/*Well, addition is too hard*/
+	return 0;
+}
